47cb7b5cc76e4ba7fdfe08f79213bbd09e0f4425
Lucas Boeing Scarduelli <lucas@scarduelli.com>
2018-03-21 12:51:23 -0300

    Aula 05 - Tratamento de Eventos  - Implementacao de mapeamento de objetos  - Implementacao de evento de clique de botao  - Implementacao de regra de tratamento do login

--- app/src/main/java/br/com/cedup/cedup20181/MainActivity.java
+++ app/src/main/java/br/com/cedup/cedup20181/MainActivity.java
@@ -2,12 +2,38 @@ package br.com.cedup.cedup20181;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botaoLogar;
+    EditText campoUsuario, campoSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoLogar = findViewById(R.id.botaoLogar);
+        campoUsuario = findViewById(R.id.campoUsuario);
+        campoSenha = findViewById(R.id.campoSenha);
+    }
+
+    public void onClickLogar(View view) {
+        String usuario = campoUsuario.getText().toString();
+        String senha = campoSenha.getText().toString();
+
+        if ("lucas".equalsIgnoreCase(usuario) && "abc123".equalsIgnoreCase(senha)) {
+            alert(getString(R.string.mensagemLoginSucesso));
+        } else {
+            alert(getString(R.string.mensagemLoginErro));
+        }
+    }
+
+    private void alert(String mensagem) {
+        Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show();
     }
 }
