5a9bde7c188db652575d241df398f0aa6c987ff5
Lucas Boeing Scarduelli <lucas@scarduelli.com>
2018-05-17 12:42:14 -0300

    Alteracoes aula 11

---
+++ app/src/main/java/br/com/cedup/cedup20181/MenuLateral2Activity.java
@@ -0,0 +1,13 @@
+package br.com.cedup.cedup20181;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuLateral2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_lateral2);
+    }
+}
